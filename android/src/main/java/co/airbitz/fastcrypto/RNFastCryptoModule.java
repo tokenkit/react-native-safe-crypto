32ab16032001d5ea7c20639eaa8140ae02be0eb6
Junzhen Wu <zhenwusw@gmail.com>
2017-08-16 13:49:26 +0800

    Integrate wg/scrypt on android

--- android/src/main/java/co/airbitz/fastcrypto/RNFastCryptoModule.java
+++ android/src/main/java/co/airbitz/fastcrypto/RNFastCryptoModule.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.lambdaworks.crypto.SCrypt;
 
 
 public class RNFastCryptoModule extends ReactContextBaseJavaModule {
@@ -22,6 +23,17 @@ public class RNFastCryptoModule extends ReactContextBaseJavaModule {
     return "RNFastCrypto";
   }
 
+  private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+  public static String bytesToHex(byte[] bytes) {
+    char[] hexChars = new char[bytes.length * 2];
+    for ( int j = 0; j < bytes.length; j++ ) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = hexArray[v >>> 4];
+      hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    }
+    return new String(hexChars);
+  }
+
   @ReactMethod
   public void scrypt(String passwd,
                      String salt,
@@ -30,6 +42,11 @@ public class RNFastCryptoModule extends ReactContextBaseJavaModule {
                      Integer p,
                      Integer size,
                      Promise promise) {
-    promise.resolve(null);
+    try {
+      byte[] result = SCrypt.scrypt(passwd.getBytes(), salt.getBytes(), 1024, 8, 1, 32);
+      promise.resolve(bytesToHex(result));
+    } catch(Exception e) {
+      promise.reject(e);
+    }
   }
 }
